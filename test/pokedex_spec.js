c6eb9699ed6032fad0cbf4ce31053c5efe5eb2be
Alek Hrycaiko <alekhrycaiko@gmail.com>
2018-03-02 16:10:39 -0800

    updated tests

--- test/pokedex_spec.js
+++ test/pokedex_spec.js
@@ -2,10 +2,21 @@ import React from 'react';
 import {shallow} from 'enzyme';
 import Pokedex from '../src/Components/Pokedex.jsx';
 import {expect} from 'chai';
+import {PokeSelector} from '../src/Components/PokeSelector.jsx';
+import {PokeDisplay} from '../src/Components/PokeDisplay.jsx';
+import {PokeText} from '../src/Components/PokeText.jsx';
 
 describe('<Pokedex /> Component Test', () => {
 	it('has a pokedisplay', () => {
 		const wrapper = shallow(<Pokedex/>);
-		expect(wrapper.find('.poke-display').exists()).to.equal(true);
+		expect(wrapper.find(PokeDisplay).exists()).to.equal(true);
+	});
+	it('has a pokeselector', () => {
+		const wrapper = shallow(<Pokedex/>);
+		expect(wrapper.find(PokeSelector).exists()).to.equal(true);
+	});
+	it('has text ', () => {
+		const wrapper = shallow(<Pokedex/>);
+		expect(wrapper.find(PokeText).exists()).to.equal(true);
 	});
 });
